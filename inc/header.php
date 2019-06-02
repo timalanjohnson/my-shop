dd550d12a0abe63801a05f0fc6dab124a13bc174
timalanjohnson <timjohnson.za@gmail.com>
2019-06-02 11:51:54 +0200

    Import bootstrap & custom styles

---
+++ inc/header.php
@@ -0,0 +1,33 @@
+<?php
+	/*
+	Timothy Johnson
+	17002289
+	WEDE6011
+	*/
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+
+	<title>My Shop</title>
+
+	<!-- Custom CSS -->
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+</head>
+<body>
+	<header>
+		<h1 class="logo-title">My Shop</h1>
+		<p>The finest selection of Fujinon X mount lenses for Fujifilm mirrorless cameras. </p>
+	</header>
+
+	<main class="container">
+
